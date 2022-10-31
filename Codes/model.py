e30895a4c82e88c75541f0dde2fc5eb19b16267e
AliFthiJ <alifathi8008@gmail.com>
2022-10-31 19:00:59 +0330

    SHHS: verfication some methods that related to model

---
+++ Codes/model.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+class model:
+    def __init__(self):
+        pass
+    def buildModel(self):
+        pass
+    def trainModel(self):
+        pass
+    def plotHist(self):
+        pass
+    def callBacks(self):
+        pass
+    def test(self):
+        pass
+    def train(self):
+        pass
+    
