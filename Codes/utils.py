9d7a2674beff19a389f716d0bce2fa744c35ae3f
AliFthiJ <alifathi8008@gmail.com>
2022-11-02 09:32:46 +0330

    SHHS: complete globForOnEdfs method

--- Codes/utils.py
+++ Codes/utils.py
@@ -13,34 +13,35 @@ class utils:
         self.signalQualIdPath = signalQualIdPath                # saves the path of signal quality id file
         self.signalQualValuePath = signalQualValuePath          # saves the path of signal quality value file
         self.signalDir = signalDir                              # path of signals
-        self.idPath = idPath
-        self.series = None
-        self.path2save = path2save
-    def readAsDF(self,signalPath,save = True):                                          # it reads signals as a CSV file
-        file = edf.EdfReader(signalPath)
-        headers = file.getSignalHeaders()
+        self.idPath = idPath                                    # location that target ids saved
+        self.path2save = path2save                              # location to save signal that converted to csv
+    def readAsDF(self,signalPath,save = False,id=None):         # it reads signals as a CSV file
+        file = edf.EdfReader(signalPath)                    
+        headers = file.getSignalHeaders()                       
         table = pd.DataFrame()
         for i in range(len(headers)):
             signal = list(file.readSignal(i))
             name = headers[i]['label']
             rate = int(headers[i]['sample_rate'])
-            chunks = [signal[x:x+rate] for x in range(0, len(signal), rate)]
+            chunks = [signal[x:x+rate] for x in range(0, len(signal), rate)]                # this line of code will chunk the main signal in term of sampling frequency
             table[name] = chunks
         if save:
-            self.saveAsCSV()
+            table.to_csv(self.path2save+'/'+str(id)+'.csv')                                 # saving every signal as csv file 
+        file.close()
         return table
     def globForOnEdfs(self):
         import glob
-        for i in glob.glob(self.signalDir+'*'):
-            id = i.split('-')[1].split('.')[0]
-            if id in self.validNormalSignalsName['id']:
-                index = self.validNormalSignalsName.loc[self.validNormalSignalsName['id']==id].index[0]
-                if (len(self.validNormalSignals['Signals'][index]) >= len(self.signalQualId-1)):
-                    pass
-            elif id in self.validPatientSignalsName['id']:
-                index = self.validPatientSignalsName.loc[self.validPatientSignalsName['id']==id].index[0]
-                if (len(self.validPatientSignalsName['Signals'][index]) >= (len(self.signalQualId)-1)):
-                    pass
+        for Dir in self.signalDir:
+            for i in glob.glob(Dir+'/*'):                                                   # for on every signal in Dir path
+                id = int(i.split('/')[-1].split('-')[1].split('.')[0])                      # extract id in the path
+                if id in list(self.validNormalSignalsName['id']):                           # condition will be True, if readed ID be our target
+                    index = self.validNormalSignalsName.loc[self.validNormalSignalsName['id']==id].index[0]
+                    if (len(self.validNormalSignalsName['Signals'][index]) >= len(self.signalQualId)-1):        # condition will be True, if every signal be valid
+                        self.readAsDF(signalPath=i)                                        
+                elif id in list(self.validPatientSignalsName['id']):
+                    index = self.validPatientSignalsName.loc[self.validPatientSignalsName['id']==id].index[0]
+                    if (len(self.validPatientSignalsName['Signals'][index]) >= (len(self.signalQualId)-1)):
+                        self.readAsDF(signalPath=i)
     def preprocessing(self):
         pass
     def readCsv(self):                                          # read needed CSV files
@@ -54,26 +55,25 @@ class utils:
         signalQualValue = signalQualValue[self.signalQualId]
         lenPatient = min(len(patient),len(absolutelyNormal))
         lenNormal = lenPatient + 200
-        self.validNormalSignalsName = self.isSignalValid(len = lenNormal,list = absolutelyNormal,qualList=signalQualValue,theresh=2,signalQualId=signalQualId)
-        self.validPatientSignalsName = self.isSignalValid(len = lenPatient,list = patient,qualList=signalQualValue,theresh=2,signalQualId=signalQualId)
+        self.validNormalSignalsName = self.isSignalValid(len = lenNormal,list = absolutelyNormal,qualList=signalQualValue,theresh=2,signalQualId=self.signalQualId)
+        self.validPatientSignalsName = self.isSignalValid(len = lenPatient,list = patient,qualList=signalQualValue,theresh=2,signalQualId=self.signalQualId)
     @staticmethod
     def isSignalValid(len,list,qualList,theresh,signalQualId):      # check that witch signals is valid to use in a EDF file
         goodSignals = pd.DataFrame(columns=['id','Signals'])    
         for i in range(len):
             id = list['nsrrid'][i]
-            # qualValue = qualList['nsrrid'][id]
             qualValue = qualList.loc[qualList['nsrrid'] == id].reset_index()
             tmpId = []
             for v  in signalQualId:
                 if int(qualValue[v][0])>theresh:
                     tmpId.append(v)
-            goodSignals.loc[i] = [id,tmpId]
+            goodSignals.loc[i] = [int(id),tmpId]
         return goodSignals
-    def saveAsCSV(self):
-        self.series.to_csv(self.path2save)    # saving every signal as csv file 
 idPath = '/home/alifathi/Documents/Projects/SHHS/Data/SHHS1.xlsx'
 signalQualIdPath = '/home/alifathi/Documents/Projects/SHHS/Data/signal quality.xlsx'
 signalQualValuePath = '/home/alifathi/Documents/Projects/SHHS/Data/datasets/1- shhs1-dataset-0.13.0.csv'
-path2save = '/run/media/alifathi/3e705399-86be-41f0-bf88-34db80a00350/CSVSeries/'
-util = utils(signalDir=None,signalQualIdPath=signalQualIdPath,signalQualValuePath=signalQualValuePath,idPath=idPath,path2save=path2save)
+path2save = '/home/alifathi/Documents/Projects/SHHS/Data/signalsAsCSV'
+signalDir = ['/run/media/alifathi/3e705399-86be-41f0-bf88-34db80a00350/shhs1','/run/media/alifathi/009ba9d2-ab8e-4caa-8e6e-e984f5895971/datapart2/part2']
+util = utils(signalDir=signalDir,signalQualIdPath=signalQualIdPath,signalQualValuePath=signalQualValuePath,idPath=idPath,path2save=path2save)
 util.readCsv()
+util.globForOnEdfs()
