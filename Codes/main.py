dc3caa22f1727801c04172f8baa6ab9a1d9169b9
alifthi <alifathi8008@gmail.com>
2022-11-05 14:12:38 -0400

    SHHS: main file developed

--- Codes/main.py
+++ Codes/main.py
@@ -1,6 +1,6 @@
 from model import model 
 from utils import utils
-
+import pandas as pd
 idPath = '~/Documents/projects/SHHS/Data/SHHS1.xlsx'
 signalQualIdPath = '~/Documents/projects/SHHS/Data/signal quality.xlsx'
 signalQualValuePath = '~/Documents/projects/SHHS/Data/datasets/1- shhs1-dataset-0.13.0.csv'
@@ -8,5 +8,7 @@ path2save = '~/Documents/Projects/SHHS/Data/signalsAsCSV'
 signalDir = ['/home/ali/Documents/projects/SHHS/Data/part2']
 util = utils(signalDir=signalDir,signalQualIdPath=signalQualIdPath,signalQualValuePath=signalQualValuePath,idPath=idPath,path2save=path2save)
 util.readCsv()
-[patientSeries,normalSeries,targets] = util.globForOnEdfs()
-trainSeries,testSetries,trainTargets,testTarget = util.preprocessing([patientSeries,normalSeries],targets)
+series = util.globForOnEdfs()
+targets = [0]*len(series[0])+[1]*len(series[1])
+df = pd.concat(series).reset_index()
+model = model()
