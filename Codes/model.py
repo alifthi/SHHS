9e5006aede05edc4301c361ebcd3740b40cda9c3
ali fthi <alifathi8008@gmail.com>
2022-12-30 14:21:16 -0800

    SHHS: load tabular data and get it to model

--- Codes/model.py
+++ Codes/model.py
@@ -34,14 +34,15 @@ class model:
         self.inputNames = inputNames
         self.freq = {'sao2': 1, 'hr': 1, 'eogl': 50 ,'eogr': 50, 'eeg': 125,'eegsec': 125,'ecg': 125,
                     'emg': 125, 'thorres': 10, 'abdores': 10, 'position': 1, 'light': 1, 'newair': 10}
-        self.len = 120 # 32010
-        self.net = None # self.buildModel()
+        self.len = 500
+        self.net = None
         self.dataFlag = True
         self.clbk = None
         self.trainData = []
         self.trainTargets = []
         self.testData = []
         self.testTargets = []
+        self.numFeatures = 13
     def buildModel(self):
         # define inputs of model
         inputs = {}                 # a dictionar that contain all inputs
@@ -53,7 +54,8 @@ class model:
         kernelSize = 3
         poolingSize = 2
         strides2D = 2
-        poolingSize2D = 2
+        stridesInputs = 2
+        poolingSize2D = 8
         ReLURate = 0.01
         dropoutRate = 0.2
         for name in self.inputNames:
@@ -63,39 +65,46 @@ class model:
 
             inputs[name+'Net'] = ksl.Input(shape = [self.len*r,1])
 
-            x = ksl.Conv1D(16,kernel_size = kernelSize,strides = 2,activation = 'relu',padding = 'same')(inputs[name+'Net'])
+            x = ksl.Conv1D(32,kernel_size = kernelSize,strides = stridesInputs,activation = 'relu',padding = 'same')(inputs[name+'Net'])
             x = ksl.MaxPooling1D(poolingSize,padding = 'same')(x)
             x = ksl.BatchNormalization()(x)
             
-            x = ksl.Conv1D(8,kernel_size = kernelSize,strides = 2,activation = 'relu',padding = 'same')(x)
+            x = ksl.Conv1D(16,kernel_size = kernelSize,strides = stridesInputs,activation = 'relu',padding = 'same')(x)
             x = ksl.MaxPooling1D(poolingSize,padding = 'same')(x)
             x = ksl.BatchNormalization()(x)
-
+            
+            x = ksl.Conv1D(8,kernel_size = kernelSize,strides = stridesInputs,activation = 'relu',padding = 'same')(x)
+            x = ksl.MaxPooling1D(poolingSize,padding = 'same')(x)
+            x = ksl.BatchNormalization()(x)
             outputs[name] = x 
         # define shared model
         concatLayer = ksl.concatenate(list(outputs.values()),axis = 1)
 
-        x = ksl.Conv1D(64,kernel_size = 3,strides = strides2D,activation = 'relu',padding = 'same')(concatLayer)
+        x = ksl.Conv1D(32,kernel_size = 3,strides = strides2D,activation = 'relu',padding = 'same')(concatLayer)
         x = ksl.MaxPooling1D(poolingSize2D,padding = 'same')(x)
         x = ksl.BatchNormalization()(x)
 
-        x = ksl.Conv1D(32,kernel_size = 3,strides = strides2D,activation = 'relu',padding = 'same')(x)
+        x = ksl.Conv1D(16,kernel_size = 3,strides = strides2D,activation = 'relu',padding = 'same')(x)
         x = ksl.MaxPooling1D(poolingSize2D,padding = 'same')(x)
         x = ksl.BatchNormalization()(x)
         
+        x = ksl.Conv1D(8,kernel_size = 3,strides = strides2D,activation = 'relu',padding = 'same')(x)
+        x = ksl.MaxPooling1D(poolingSize2D,padding = 'same')(x)
+        x = ksl.BatchNormalization()(x)   
 
         x = ksl.Flatten()(x)
         
         featureInput = ksl.Input(shape = [self.numFeatures])
-        y = ksl.Dense(128,ativation = 'relu')(featureInput)
-        y = ksl.LeakyRelu(featureReluRate)(y)
+        y = ksl.Dense(128,activation = 'relu')(featureInput)
+        y = ksl.LeakyReLU(featureReluRate)(y)
         y = ksl.Dropout(featureDropoutRate)(y)
 
-        y = ksl.Dense(256,ativation = 'relu')(featureInput)
-        y = ksl.LeakyRelu(featureReluRate)(y)
+        y = ksl.Dense(256,activation = 'relu')(featureInput)
+        y = ksl.LeakyReLU(featureReluRate)(y)
         y = ksl.Dropout(featureDropoutRate)(y)
 
-        x = ksl.concatenate([x,y],axis = 0)
+        x = ksl.concatenate([x,y],axis = 1)
+
         x = ksl.Dense(32)(x)
         x = ksl.LeakyReLU(ReLURate)(x)
         x = ksl.Dropout(dropoutRate)(x)
@@ -105,11 +114,11 @@ class model:
         x = ksl.Dropout(dropoutRate)(x)
         
         output = ksl.Dense(1,activation = 'sigmoid')(x)
-        return tf.keras.Model(inputs = list(inputs.values()),outputs = output)
+        return tf.keras.Model(inputs = list(inputs.values())+[featureInput],outputs = output)
     def transformer(self):
         pass
     def compile(self,saveAddr):
-        opt = optim.SGD(lr=0.1)  # we can use lr schedual
+        opt = optim.SGD(lr=0.01)  # we can use lr schedual
         Loss = loss.BinaryCrossentropy()
         self.net.compile(optimizer = opt,loss = 'binary_crossentropy',metrics = ['accuracy'])
         self.net.summary()
@@ -119,42 +128,42 @@ class model:
                     validation_data=[validationData,validationTargets],callbacks = self.clbk)
         return hist
     def trainGenerator(self,util,saveAddr,split = False):
-        normalLen = 100
-        patientLen = 100
+        normalLen = 180
+        patientLen = 180
         numFiles = 830
-        epochs = 10
-        steps = int(numFiles/patientLen)
+        epochs = 1
+        steps = 1 # int(numFiles/patientLen)
         for i in range(epochs):
             print(f'epoch {i}:\n')
 
             for j in range(steps):
-                if not self.dataFlag and j == len(self.trainData):
+                if not self.dataFlag and j == (len(self.trainData)):
                     break
                 if self.dataFlag:
-                    try:
-                        if split:
+                     try:
+                        if split == True :
                             trainD,trainT,testD,testT = util.dataGenerator(normalLen = normalLen,
                                                                        patientLen = patientLen,split = True)
-                            self.testData.append(testD)
+                            self.testnData.append(testD)
                             self.testTargets.append(testT)
-                        else:
-                            trainD,trainT,testD,testT = util.dataGenerator(normalLen = normalLen,
+                        elif split == False :
+                            trainD,trainT = util.dataGenerator(normalLen = normalLen,
                                                                          patientLen = patientLen)
                         self.trainData.append(trainD)
                         self.trainTargets.append(trainT)
-                    except:
+                     except:
                         self.dataFlag = False
                         break
 
                      
                 print(f'step: {j}')
                 self.callBacks(epoch = 'epoch_' + str(i) + '_step_' +str(j))
-                hist = self.net.fit(self.trainData[j],self.trainTargets[j],epochs=15,batch_size=128,
+                hist = self.net.fit(self.trainData[j],self.trainTargets[j],epochs=30,batch_size=32,
                                     validation_data=[self.testData,self.testTargets],callbacks = self.clbk)
-                self.callBacks(epoch = 'epoch_' + str(i) + '_step_' +str(j))
-                self.plotHist(hist,saveAddr = r'~/Documents/projects/SHHS/Plots',
+               
+                self.plotHist(hist,saveAddr = r'C:\Users\u23\Documents\project\Plots',
                               i ='_epoch_' + str(i) + '_step_' +str(j) )
-                with open(f"~/Documents/project/History/historys{j}e{i}.txt") as f: 
+                with open(f"C:\\Users\\u23\\Documents\\project\\History\\History\\historys{j}e{i}.txt", 'w') as f: 
                     for key, value in hist.history.items(): 
                         f.write(f'epochs {i} :\n Key:  {key}  Value:  {value}')
                     f.write('\n####################################################')
@@ -165,23 +174,25 @@ class model:
         plt.plot(Hist.history['accuracy'])
         plt.plot(Hist.history['val_accuracy'])
         plt.title('model accuracy')
-        plt.savefig(saveAddr +'accuracy_'+i+'.png')
+        plt.savefig(saveAddr +'\\accuracy_'+i+'.png')
+        plt.close()
         plt.plot(Hist.history['loss'])
         plt.plot(Hist.history['val_loss'])
         plt.title('model loss')
-        plt.savefig(saveAddr + 'loss_'+i+'.png')
+        plt.savefig(saveAddr + '\\loss_'+i+'.png')
+        plt.close()
     def callBacks(self,epoch):
         self.clbk = []
-        clbk = tf.keras.callbacks.ModelCheckpoint(filepath = r'~/Documents/projects/SHHS/callBacks/modelCheckpoint',
+        clbk = tf.keras.callbacks.ModelCheckpoint(filepath = r'C:\Users\u23\Documents\project\callBacksFile\modelCheckpoint\model.h5',
                                                         monitor = 'val_accuracy',
                                                         mode='max',
                                                         save_best_only=True)
         self.clbk.append(clbk)
         
-        clbk = tf.keras.callbacks.TensorBoard(log_dir = r'~/Documents/projects/SHHS/callBacks/tensorboard'+'/'+ epoch +'_log')
+        clbk = tf.keras.callbacks.TensorBoard(log_dir = r'C:\Users\u23\Documents\project\callBacksFile\tensorboard'+'\\'+ epoch +'_log')
         self.clbk.append(clbk)
         
-        clbk = tf.keras.callbacks.EarlyStopping()
+        clbk = tf.keras.callbacks.EarlyStopping(patience = 3)
         self.clbk.append(clbk)
         
     def loadModel(self,addr):
--- Codes/utils.py
+++ Codes/utils.py
@@ -30,22 +30,19 @@ class utils:
                 outlayerPath,featurePath):
         
         self.outlayerPath = outlayerPath
+        self.featurePath = featurePath
         self.signalQualIdPath = signalQualIdPath                # saves the path of signal quality id file
         self.signalQualValuePath = signalQualValuePath          # saves the path of signal quality value file
         self.signalDir = signalDir                              # path of signals
         self.idPath = idPath                                    # location that target ids saved
         self.path2save = path2save                              # location to save signal that converted to csv
         self.targetSignals = targetSignals
-        self.featurePath = featurePath
         self.freq = {'sao2': 1, 'hr': 1, 'eogl': 50 ,'eogr': 50, 'eeg': 125,'eegsec': 125,'ecg': 125,
                     'emg': 125, 'thorres': 10, 'abdores': 10, 'position': 1, 'light': 1, 'newair': 10}
-        self.len = 120 # 32010
+        self.len = 500 
         self.namesFilter = '.,_ ()'
         self.readSignals  = []                  #  this list contain data that read
         self.invalidSignals = []
-    def readFeatures(self,id):
-
-        pass
     def readAsDF(self,signalPath,save = False,id=None,returnOneSignal = False):         # it reads signals as a CSV file
         file = edf.EdfReader(signalPath)                    
         headers = file.getSignalHeaders() 
@@ -72,16 +69,15 @@ class utils:
                 name = name.replace(c,'')
                 name = name.lower()
             names[i] = name
-        for name in self.targetSignals: # range(len(names)):
+        for name in self.targetSignals:
 
             if not(name in names):
                 return None
             i = names.index(name)
             rate = int(headers[i]['sample_rate'])
             lowerRate = self.len
-            nHoure = 3
             signal = list(file.readSignal(i))
-            chunks = [signal[x:x+rate] for x in range(0, rate*nHoure*3600, rate)]                # this line of code will chunk the main signal in term of sampling frequency
+            chunks = [signal[x:x+rate] for x in range(0, rate*self.len, rate)]                # this line of code will chunk the main signal in term of sampling frequency
             lowerChunk = [chunks[x:x+lowerRate] for x in range(0, len(chunks), lowerRate)]
             if returnOneSignal:  
                 table[name] = lowerChunk
@@ -90,7 +86,7 @@ class utils:
         if save:
             table.to_csv(self.path2save+'\\'+str(id)+'.csv')                                 # saving every signal as csv file 
         file.close()
-        if returnOneSignal: # and len(table.columns) == len(self.targetSignals):                     
+        if returnOneSignal:                    
             return table
         elif (not returnOneSignal) and len(List) == len(self.targetSignals):
             return List
@@ -99,8 +95,6 @@ class utils:
         
     def globForOnEdfs(self,normalLen=10,patientLen=10):
         import glob
-        normalLen -=1
-        patientLen -=1
         normalData = []
         patientData = []
         normalFeatures = []
@@ -119,14 +113,19 @@ class utils:
                     if (len(self.validNormalSignalsName['Signals'][index]) >= len(self.signalQualId)-1):        # condition will be True, if every signal be valid
                         if(normalLen == normalCounter):
                             if (patientLen == patientCounter):
+                                normalFeatures = pd.concat(normalFeatures,axis=0,ignore_index=True)
+                                patientFeatures = pd.concat(patientFeatures,axis=0,ignore_index=True)
                                 normalData = pd.concat(normalData,axis=0,ignore_index=True)
                                 patientData = pd.concat(patientData,axis=0,ignore_index=True)
                                 return [normalData,patientData,patientFeatures,normalFeatures]
                             else:
                                 continue
                         try :
-                            
                             signal = self.readAsDF(signalPath=path,id = id,returnOneSignal=True) 
+                            feature = self.normalFeatures.loc[self.normalFeatures['nsrrid'] == id].reset_index()
+                            if feature.shape[0] == 0:
+                                self.invalidSignals.append(id)
+                                continue
                         except:
                             continue
                         try:
@@ -138,14 +137,17 @@ class utils:
                             pass
                         if normalCounter%10 == 0:
                             print(f'normal: {normalCounter}')
+
+
                         self.readSignals.append(id)
-                        feature = self.normalFeatures.loc[self.normalFeatures['nsrrid'] == id].to_numpy()
                         normalFeatures.append(feature)
                         normalData.append(signal)
                         normalCounter +=1 
                 elif id in list(self.validPatientSignalsName['id']):
                     if  (patientLen == patientCounter):
                         if (normalLen == normalCounter) :
+                            normalFeatures = pd.concat(normalFeatures,axis=0,ignore_index=True)
+                            patientFeatures = pd.concat(patientFeatures,axis=0,ignore_index=True)
                             normalData = pd.concat(normalData,axis=0,ignore_index=True)
                             patientData = pd.concat(patientData,axis=0,ignore_index=True)
                             return [normalData,patientData,patientFeatures,normalFeatures]
@@ -154,22 +156,27 @@ class utils:
                     index = self.validPatientSignalsName.loc[self.validPatientSignalsName['id']==id].index[0]
                     if (len(self.validPatientSignalsName['Signals'][index]) >= (len(self.signalQualId)-1)):
                         signal = self.readAsDF(signalPath=path,returnOneSignal=True)
+                        feature = self.patientFeatures.loc[self.patientFeatures['nsrrid'] == id].reset_index()
+                        if feature.shape[0] == 0:
+                            self.invalidSignals.append(id)
+                            continue                        
                         try:
                             if signal == None:
                                 if not (id in self.invalidSignals):
                                        self.invalidSignals.append(id)
-                                       
+                                    
                                 continue
                         except:
                             pass
                         if patientCounter%10 == 0:
                             print(f'patient: {patientCounter}')
                         self.readSignals.append(id)
-                        feature = self.patientFeatures.loc[self.patientFeatures['nsrrid'] == id].to_numpy()
                         patientFeatures.append(feature)
                         patientData.append(signal)
                         patientCounter +=1
         try:
+            normalFeatures = pd.concat(normalFeatures,axis=0,ignore_index=True)
+            patientFeatures = pd.concat(patientFeatures,axis=0,ignore_index=True)
             normalData = pd.concat(normalData,axis=0,ignore_index=True)
             patientData = pd.concat(patientData,axis=0,ignore_index=True)
             normalData = normalData.T
@@ -198,19 +205,18 @@ class utils:
             data = np.concatenate(data,axis=1)
             mainData.append(data)
         return [mainData,Targets]
-    def preprocessing(self,series,targets,addNoise = False,split = True):
+    def preprocessing(self,series,targets,features,addNoise = False,split = True):
+        from sklearn.model_selection import train_test_split
         for i in range(len(series)):
             for j in range(len(self.targetSignals)):
                 m = max(max(series[i][j]),1)
                 for z in range(len(series[i][j])):
                     series[i][j][z] /= m
         if split:
-            from sklearn.model_selection import train_test_split
             trainSeries,testSetries,trainTargets,testTarget = train_test_split(series,targets,test_size = 0.1)
-            if addNoise:            
-                trainSeries = trainSeries + np.random.normal(0,1,np.shape(trainSeries))
             return [trainSeries,testSetries,trainTargets,testTarget]
-        return [series,targets]
+        series,_,features,_,targets,_ = train_test_split(series,features,targets,test_size = 0.00001)
+        return [series,features,targets]
     def readCsv(self):                                          # read needed CSV files
         patient = pd.read_excel(self.idPath,sheet_name='Patient')
         absolutelyNormal = pd.read_excel(self.idPath,sheet_name='Absolutely Normal')
@@ -239,7 +245,7 @@ class utils:
                                                           signalQualId=self.signalQualId,
                                                           outlayerValue = outlayerValue,
                                                           outlayerID = self.outlayerID)
-        self.normalFeatures = pd.read_excel(self.featurePath+'normal.xlsx')
+        self.normalFeatures = pd.read_excel(self.featurePath+'absNormal.xlsx')
         self.patientFeatures = pd.read_excel(self.featurePath+'patient.xlsx')
     @staticmethod
     def isSignalValid(Len,list,qualList,thereshQual,signalQualId,outlayerValue,outlayerID):      # check that witch signals is valid to use in a EDF file
@@ -262,28 +268,60 @@ class utils:
             array = []
             for i in self.targetSignals :   
                 shape = np.shape(Data[i][j])
-                tmpArray = np.reshape(Data[i][j],[shape[1]*shape[0],1])
+                tmpArray = np.reshape(Data[i][j],[shape[1]*shape[0]])
+                tmpArray=np.expand_dims(tmpArray,axis = -1)
                 array.append(tmpArray)
             data.append(array)
         return data
-    def dataGenerator(self,normalLen,patientLen):
+    def dataGenerator(self,normalLen,patientLen,split = False):
         
         self.readCsv()
-        [normalData,patientData] = self.globForOnEdfs(normalLen=normalLen,patientLen=patientLen)
+        [normalData,patientData,patientFeature,normalFeature] = self.globForOnEdfs(normalLen=normalLen,patientLen=patientLen)
         print('preparing data...')
-        if len(normalData) <= 15:
-            normalData = normalData.T
-            patientData = patientData.T
+
+            
+        patientFeature = patientFeature.drop('index',axis =1 )
+        normalFeature = normalFeature.drop('index',axis =1 )
+        
         trainTargets = [0]*(len(normalData))+[1]*(len(patientData))
+        featureData = pd.concat([normalFeature,patientFeature],axis = 0,ignore_index = True)
+        featureData = featureData.dropna()
+        featureData = featureData.reset_index()
+        featureData = featureData.drop(['index','nsrrid'],axis =1 ).to_numpy()
+
         Data = pd.concat([normalData,patientData],axis=0,ignore_index=True) 
         Data = Data.dropna()
         Data = Data.reset_index()
         Data = Data.drop('index',axis = 1)
+        print(Data.shape)
         Data.columns = self.targetSignals
         del(normalData,patientData)
         trainData = self.squeeze(Data)
         del(Data)
-        trainData,testData,trainTargets,testTargets = self.preprocessing(series=trainData,targets=trainTargets,split = True)
+        if split:
+            trainData,testData,trainTargets,testTargets = self.preprocessing(series=trainData,
+                                                                             targets=trainTargets,
+                                                                             addNoise=True,
+                                                                             split = False)
+            [trainData,trainTargets] = self.prepareData(Data = trainData,targets = trainTargets)
+            trainT = np.squeeze(trainTargets)
+            trainD = []
+            for i,d in enumerate(trainData):
+                d = d.reshape([np.shape(d)[1],self.len*self.freq[self.targetSignals[i]]])
+                trainD.append(d)
+            del(trainData,trainTargets)
+            [testData,trainTargets] = self.prepareData(Data = testData,targets = testTargets)
+            testT = np.squeeze(trainTargets)
+            testD = []
+            for i,d in enumerate(testData):
+                d = d.reshape([np.shape(d)[1],self.len*self.freq[self.targetSignals[i]]])
+                testD.append(d) 
+            return [trainD,trainT,testD,testT]
+
+        trainData,featureData,trainTargets = self.preprocessing(series=trainData,targets=trainTargets,
+                                                            features = featureData,
+                                                            addNoise = False,
+                                                            split = False)
         [trainData,trainTargets] = self.prepareData(Data = trainData,targets = trainTargets)
         trainT = np.squeeze(trainTargets)
         trainD = []
@@ -291,10 +329,5 @@ class utils:
             d = d.reshape([np.shape(d)[1],self.len*self.freq[self.targetSignals[i]]])
             trainD.append(d)
         del(trainData,trainTargets)
-        [testData,trainTargets] = self.prepareData(Data = testData,targets = testTargets)
-        testT = np.squeeze(trainTargets)
-        testD = []
-        for i,d in enumerate(testData):
-            d = d.reshape([np.shape(d)[1],self.len*self.freq[self.targetSignals[i]]])
-            testD.append(d) 
-        return [trainD,trainT,testD,testT]
+        trainD.append(featureData)
+        return [trainD,trainT]
